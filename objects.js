90e9590094acfcf13044e4831597daffcff2c663
Josh <joshuakettle1990@gmail.com>
2017-10-12 06:33:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {'artistName' : 'songTitles'}
 var updatePlaylist= function(playlist, artistName, songName){
   return Object.assign({}, playlist, {[artistName] : songName})
 }
+
+var removeFromPlaylist = function(playlist, artistName){
+  
+}
